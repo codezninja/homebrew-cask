d3306f6f416f655d6e4526285ac5464eabb2bf92
Ezbon Jacob <ezjacob@gmail.com>
2023-01-20 09:26:07 -0600

    feat: add new cask for keybase-arm64 app

---
+++ Casks/keybase-arm64.rb
@@ -0,0 +1,54 @@
+cask "keybase-arm64" do
+  version "6.0.4,20230113195947,6e1343b357"
+  sha256 "f1bd79665b11a392f1b48c595e42442f593126ed0ffe1c862c71d7f7de87f99d"
+
+  url "https://prerelease.keybase.io/darwin-arm64-updates/Keybase-#{version.csv.first}-#{version.csv.second}%2B#{version.csv.third}.zip"
+  name "Keybase"
+  desc "End-to-end encryption software"
+  homepage "https://keybase.io/"
+
+  livecheck do
+    url "https://prerelease.keybase.io/update-darwin-arm64-prod-v2.json"
+    strategy :page_match do |page|
+      match = page.match(/Keybase[._-]v?(\d+(?:\.\d+)+)[._-](\d+)%2B([0-9a-f]+)\.zip/i)
+      next if match.blank?
+
+      "#{match[1]},#{match[2]},#{match[3]}"
+    end
+  end
+
+  auto_updates true
+  depends_on arch: :arm64
+
+  app "Keybase.app"
+
+  postflight do
+    system_command "#{appdir}/Keybase.app/Contents/SharedSupport/bin/keybase",
+                   args: ["install-auto"]
+  end
+
+  uninstall delete:    [
+              "/Library/Logs/keybase*",
+              "/Library/PrivilegedHelperTools/keybase.Helper",
+            ],
+            launchctl: "keybase.Helper",
+            signal:    [
+              ["TERM", "keybase.Electron"],
+              ["TERM", "keybase.ElectronHelper"],
+              ["KILL", "keybase.Electron"],
+              ["KILL", "keybase.ElectronHelper"],
+            ],
+            script:    {
+              executable: "#{appdir}/Keybase.app/Contents/SharedSupport/bin/keybase",
+              args:       ["uninstall"],
+            }
+
+  zap trash: [
+        "~/Library/Application Support/Keybase",
+        "~/Library/Caches/Keybase",
+        "~/Library/Group Containers/keybase",
+        "~/Library/Logs/Keybase*",
+        "~/Library/Preferences/keybase*",
+      ],
+      rmdir: "/keybase"
+end
